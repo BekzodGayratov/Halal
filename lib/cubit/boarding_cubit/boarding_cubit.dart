79b79fabaa8267b8e651f747a41e7f4169a1f6e0
BekzodGayratov <bekzodflutter@gmail.com>
2022-04-01 08:01:53 +0500

    boarding page was completed

--- lib/cubit/boarding_cubit/boarding_cubit.dart
+++ lib/cubit/boarding_cubit/boarding_cubit.dart
@@ -1,12 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:halal/cubit/boarding_cubit/boarding_state.dart';
 
 class BoardingCubit extends Cubit<BoardingState> {
   int index = 0;
+  int currentItemIndex = 0;
+  Color color = const Color(0xff058F1A);
   BoardingCubit() : super(InitialState());
 
   void changeBoarding(int indexAt) {
     index = indexAt;
+    emit(BoardingChangeState());
+  }
+
+  void changeCurrentItemIndex(int index) {
+    currentItemIndex = index;
     emit(BoardingChangeState());
   }
 }
--- lib/view/pages/boarding_pages.dart
+++ lib/view/pages/boarding_pages.dart
@@ -5,7 +5,6 @@ import 'package:halal/coponents/my_primary_button.dart';
 import 'package:halal/cubit/boarding_cubit/boarding_cubit.dart';
 import 'package:halal/cubit/boarding_cubit/boarding_state.dart';
 import 'package:halal/functions/show_boarding_border.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BoardingPage extends StatefulWidget {
   const BoardingPage({Key? key}) : super(key: key);
@@ -33,7 +32,7 @@ class _BoardingPageState extends State<BoardingPage>
   myScaffold(context) {
     return Scaffold(
         body: BlocBuilder<BoardingCubit, BoardingState>(
-      builder: (ctx, state) => SafeArea(
+      builder: (context, state) => SafeArea(
         child: TabBarView(
           controller: _controller,
           children: List.generate(
@@ -54,33 +53,70 @@ class _BoardingPageState extends State<BoardingPage>
                   ),
                 ),
                 Expanded(
-                  flex: 6,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        width: double.infinity,
-                        child: Text(
-                          boardingTitles[index],
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 26),
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 2,
+                  flex: 5,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width *0.02),
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: MediaQuery.of(context).size.height *0.04,
                         ),
-                      ),
-                      Row(
-                        children: List.generate(
-                            3,
-                            (index) => CircleAvatar(
-                                  radius: 5.0,
+                        SizedBox(
+                          width: double.infinity,
+                          child: Text(
+                            boardingTitles[index],
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w600, fontSize: 26),
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: MediaQuery.of(context).size.height *0.08),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: List.generate(3, (index) {
+                              context
+                                  .read<BoardingCubit>()
+                                  .changeCurrentItemIndex(index);
+                              return Container(
+                                margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width *0.01),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    width: 1.5,
+                                    color: const Color(0xff058F1A),
+                                  ),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: CircleAvatar(
+                                  radius: 4.0,
                                   backgroundColor:
-                                      ctx.watch<BoardingCubit>().index == index
+                                      context.watch<BoardingCubit>().index ==
+                                              context
+                                                  .watch<BoardingCubit>()
+                                                  .currentItemIndex
                                           ? const Color(0xff058F1A)
                                           : Colors.transparent,
-                                )),
-                      ),
-                      MyPrimaryButton(
-                          child: const Text("Keyingisi"), onPressed: () {})
-                    ],
+                                ),
+                              );
+                            }),
+                          ),
+                        ),
+                        MyPrimaryButton(
+                            child: const Text("Keyingisi"),
+                            onPressed: () {
+                              if (_controller!.previousIndex !=1) {
+                                _controller!.index += 1;
+                                context
+                                    .read<BoardingCubit>()
+                                    .changeBoarding(_controller!.index);
+                              } else {
+                                print("Hello");
+                              }
+                            })
+                      ],
+                    ),
                   ),
                 ),
               ],
