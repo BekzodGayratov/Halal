81b1072edc29061baac9bfb701990877226cb55d
BekzodGayratov <bekzodflutter@gmail.com>
2022-04-03 15:30:47 +0500

    live page was completed

--- lib/view/pages/live_page.dart
+++ lib/view/pages/live_page.dart
@@ -2,8 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:halal/coponents/my_back_button_comp.dart';
 
-class LivePage extends StatelessWidget {
+class LivePage extends StatefulWidget {
   const LivePage({Key? key}) : super(key: key);
+
+  @override
+  State<LivePage> createState() => _LivePageState();
+}
+
+class _LivePageState extends State<LivePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(microseconds: 300)).then((value) {
+      showProVersionDialog(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +38,12 @@ class LivePage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         height: double.infinity,
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage("assets/animals/live.png",),fit: BoxFit.cover)
-        ),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                  "assets/animals/live.png",
+                ),
+                fit: BoxFit.cover)),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Column(
@@ -39,11 +56,43 @@ class LivePage extends StatelessWidget {
               Icons.logout_outlined,
               color: Colors.white,
             ),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
-          const Text("Efirdan chiqish",style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w400),)
+          const Text(
+            "Efirdan chiqish",
+            style: TextStyle(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.w400),
+          )
         ],
       ),
     );
+  }
+
+  showProVersionDialog(context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.0),
+                  color: const Color(0xffF5F5F5)),
+              child: Column(
+                children: [
+                  SvgPicture.asset("assets/splash.svg"),
+                  const Text(
+                      "Jonli onlayn kuzatish uchun Pro versiyaga o'tishingiz kerak"),
+                  const Divider(),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text("Pro versiyaga o'tish"))
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
